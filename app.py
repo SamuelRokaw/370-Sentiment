2e353512c589d1d9068b3752a309890302c2a1c3
Samuel Rokaw <143557677+SamuelRokaw@users.noreply.github.com>
2025-05-06 13:53:16 -0700

    a

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 from Sentimentanal import analyze_sentiment  # Import the function from Sentimentanal.py
 import os  # Import os to access environment variables
+import nltk
 
 app = Flask(__name__)
 
@@ -16,5 +17,7 @@ def form():
     return render_template("form.html", output=output, user_input=user_input)  # returns the analysis result and user input so they can be displayed
 
 if __name__ == "__main__":
+    nltk.download('punkt')
     port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or default to 5000
     app.run(host="0.0.0.0", port=port)  # Listen on all interfaces
+    
