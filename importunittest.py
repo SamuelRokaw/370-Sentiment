46ace479a4a318a6e9ca563878547bf6be7122b5
Samuel Rokaw <143557677+SamuelRokaw@users.noreply.github.com>
2025-05-10 20:37:20 -0700

    more unit tests

--- importunittest.py
+++ importunittest.py
@@ -6,18 +6,38 @@ from Sentimentanal import analyze_sentiment
 class TestSentimentAnalysis(unittest.TestCase):
     def test_positive_sentiment(self):
         result = analyze_sentiment("I love this product, it is amazing!")
-        self.assertEqual(result, "positive")
+        self.assertEqual(result, "positive") # Expected to be positive
 
     def test_negative_sentiment(self):
         result = analyze_sentiment("I hate this product, it is terrible!")
-        self.assertEqual(result, "negative")
+        self.assertEqual(result, "negative") # Expected to be negative
 
     def test_neutral_sentiment(self):
         result = analyze_sentiment("This product is okay, nothing special.")#will fail because model isn't trained on neutral
-        self.assertEqual(result, "neutral")
+        self.assertEqual(result, "neutral") # Expected to be neutral
 
     def test_not_handling(self):
         result = analyze_sentiment("This product is not good.")
+        self.assertEqual(result, "negative")  # Expected to be negative
+
+    def test_not_handling_positive(self):
+        result = analyze_sentiment("This product is not bad.")
+        self.assertEqual(result, "positive")  # Expected to be positive
+    
+    def test_double_negative(self):
+        result = analyze_sentiment("This product is not not good.")
+        self.assertEqual(result, "positive")  # Expected to be positive
+
+    def test_double_negative(self):
+        result = analyze_sentiment("This product is not not bad.")
+        self.assertEqual(result, "negative")  # Expected to be negative
+    
+    def test_long_sentence_1(self):
+        result = analyze_sentiment("if you are thinking of buying this, don't buy it")
+        self.assertEqual(result, "negative")  # Expected to be negative
+    
+    def test_long_sentence_2(self):
+        result = analyze_sentiment("if you are thinking of buying this, don't")
         self.assertEqual(result, "negative")  # Expected to be negative
 
 if __name__ == "__main__":
